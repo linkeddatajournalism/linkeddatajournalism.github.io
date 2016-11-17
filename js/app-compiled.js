09effe5278ab9201498f6790176df30f317f3a98
reinvantveer <rein.van.t.veer@geodan.nl>
2016-11-17 16:30:30 +0100

    Graph loader and execution tied together. Working app.

--- js/app-compiled.js
+++ js/app-compiled.js
@@ -1,30 +1,54 @@
-"use strict";
+/**
+ * Created by reinv on 10-11-2016.
+ */
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-/**
- * Created by reinv on 10-11-2016.
- */
+var mimeType = 'text/turtle';
+
+var journalismGraph = rdfstore.create(function (err, store) {
+  if (err) return console.error(err);
+  return store;
+});
+
 var LDJapp = function () {
   function LDJapp() {
     _classCallCheck(this, LDJapp);
   }
 
   _createClass(LDJapp, [{
-    key: "bindLoadDataButton",
+    key: 'bindLoadDataButton',
     value: function bindLoadDataButton(buttonId, dataDivId) {
-      $("#" + buttonId).click(function () {
-        var data = $("#" + dataDivId).text();
-        console.log(data);
+      $('#' + buttonId).click(function () {
+        $('#' + buttonId).text('Loading...');
+        var data = $('#' + dataDivId).val();
+        journalismGraph.load(mimeType, data, function (err, numberOfTriples) {
+          if (err) {
+            $('#' + buttonId).text('There was an error (see console log)');
+            return console.error(err);
+          }
+          $('#' + buttonId).text(numberOfTriples + ' triples loaded!');
+          console.log(numberOfTriples + ' triples loaded');
+        });
       });
     }
   }, {
-    key: "bindQueryButton",
+    key: 'bindQueryButton',
     value: function bindQueryButton(buttonId, yasqe) {
-      $("#" + buttonId).click(function () {
-        console.log(yasqe.getValue());
+      $('#' + buttonId).click(function () {
+        var sparqlQuery = yasqe.getValue();
+        console.log(sparqlQuery);
+        journalismGraph.execute(sparqlQuery, function (status, results) {
+          console.log(status);
+          if (results.length) {
+            console.log(results);
+          } else {
+            alert('The query yielded no results');
+          }
+        });
       });
     }
   }]);
--- js/app.js
+++ js/app.js
@@ -1,17 +1,42 @@
 /**
  * Created by reinv on 10-11-2016.
  */
+'use strict';
+const mimeType = 'text/turtle';
+
+const journalismGraph = rdfstore.create((err, store) => {
+  if (err) return console.error(err);
+  return store;
+});
+
 class LDJapp {
   bindLoadDataButton(buttonId, dataDivId) {
     $(`#${buttonId}`).click(() => {
-      const data = $(`#${dataDivId}`).text();
-      console.log(data);
+      $(`#${buttonId}`).text('Loading...');
+      const data = $(`#${dataDivId}`).val();
+      journalismGraph.load(mimeType, data, (err, numberOfTriples) => {
+        if (err) {
+          $(`#${buttonId}`).text('There was an error (see console log)');
+          return console.error(err);
+        }
+        $(`#${buttonId}`).text(numberOfTriples + ' triples loaded!');
+        console.log(numberOfTriples + ' triples loaded');
+      });
     });
   }
 
   bindQueryButton(buttonId, yasqe) {
     $(`#${buttonId}`).click(() => {
-      console.log(yasqe.getValue());
+      const sparqlQuery = yasqe.getValue();
+      console.log(sparqlQuery);
+      journalismGraph.execute(sparqlQuery, (status, results) => {
+        console.log(status);
+        if (results.length) {
+          console.log(results);
+        } else {
+          alert('The query yielded no results');
+        }
+      });
     });
   }
 }
